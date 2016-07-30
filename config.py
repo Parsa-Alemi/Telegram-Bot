986db40df88e37b4321569107cc7805edcc9dae9
Unf-Team <unf.team1@gmail.com>
2016-07-30 12:04:39 +0430

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,6 @@
 
-token = 'Your Token' # Add Your Token
-is_sudo = 'ID Admin' # add Your ID
+token = 'Your Token' 
+is_sudo = 'ID Admin' 
 
-
-
-# _____           _              _____
-#|_   _|_ _ _   _| | ___  _ __  |_   _|__  __ _ _ __ ___
-#  | |/ _` | | | | |/ _ \| '__|   | |/ _ \/ _` | '_ ` _ \
-#  | | (_| | |_| | | (_) | |      | |  __/ (_| | | | | | |
-#  |_|\__,_|\__, |_|\___/|_|      |_|\___|\__,_|_| |_| |_|
-#           |___/
-#Copy right  2016 Negative - Taylor Team
-#MIT license
+#UNF-TEAM copyright 2016
+#dev by parsa alemi
